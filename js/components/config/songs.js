bbac81c5af6dd3f8e992659b23f381ffc627c603
Kevin <kevinduss1@gmail.com>
2022-03-21 19:38:53 +0100

    add songs

--- js/components/config/songs.js
+++ js/components/config/songs.js
@@ -1,19 +1,19 @@
+const propagandalf = {
+    title: 'propagandalf',
+    cover: new URL('/img/covers/propagandalf.jpg', import.meta.url),
+    song: new URL('/mp3/propagandalf.mp3', import.meta.url),
+    href: new URL('https://open.spotify.com/track/2nhJ3KZlUoUSza8vxbDohm?si=efb14158f44f4855', import.meta.url),
+    color: 0xff2a60,
+    colorHex: '#ff2a60'
+};
+
 const jekami = {
     title: 'jekami',
     cover: new URL('/img/covers/jekami.jpg', import.meta.url),
     song: new URL('/mp3/jekami.mp3', import.meta.url),
-    href: new URL('https://open.spotify.com/album/2Svs2VEWvwCdSa0dszmUXl', import.meta.url),
+    href: new URL('https://open.spotify.com/track/3vKhPuZs9CbZJY54tnIK2X?si=91d614b909ce46ee', import.meta.url),
     color: 0xe0e100,
     colorHex: '#e0e100'
-};
-
-const tanzt = {
-    title: 'tanzt',
-    cover: new URL('/img/covers/tanzt.jpg', import.meta.url),
-    song: new URL('/mp3/tanzt.mp3', import.meta.url),
-    href: undefined,
-    color: 0xd60000,
-    colorHex: '#d60000'
 };
 
 const freieEnergie = {
@@ -26,10 +26,10 @@ const freieEnergie = {
 };
 
 export default new Map([
-    [0, jekami],
-    [1, jekami],
-    [2, tanzt],
-    [3, tanzt],
+    [0, propagandalf],
+    [1, propagandalf],
+    [2, jekami],
+    [3, jekami],
     [4, freieEnergie],
     [5, freieEnergie]
 ]);
